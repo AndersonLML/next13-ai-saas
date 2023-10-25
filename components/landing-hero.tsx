3d6cc9963a89e436b825185ce5345e1586a525fb
Anderson Leite <68247603+AndersonLML@users.noreply.github.com>
2023-10-25 01:53:31 -0300

    maisumup

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,15 +12,12 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>A melhor ferramenta de IA para</h1>
+        <h1>A melhor ferramenta de I.A para</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <TypewriterComponent
             options={{
               strings: [
-                "Chatbot.",
-                "Geração de Fotos.",
-                "Redação de Blogs.",
-                "Escrita de Textos."
+                "Chat com I.A (Seu ChatGPT)",
               ],
               autoStart: true,
               loop: true,
